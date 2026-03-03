9cefa2859cf734df6092c092d3b43e9d82f77366
José Rodolfo <rdfvalerio@gmail.com>
2026-03-03 16:47:28 -0300

    feat: adiciona schemas de sessão e verificação de tokens

---
+++ infra/database/schema/index.ts
@@ -0,0 +1,9 @@
+import { sessionSchema } from "./sessions";
+import { usersSchema } from "./users";
+import { verificationTokensSchema } from "./verificationTokens";
+
+export const schema = {
+  usersSchema,
+  sessionSchema,
+  verificationTokensSchema,
+};
