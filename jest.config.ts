8a79db9a55061965b6ddadac2bf4cd3e5d9b74e4
José Rodolfo <rdfvalerio@gmail.com>
2026-03-03 15:25:29 -0300

    chore: adiciona `jest.config.ts`

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: `.env.development` });
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  coverageProvider: "v8",
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testTimeout: 60000,
+};
+
+export default createJestConfig(config);
