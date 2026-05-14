afcad4f27d9b1acd5fbc7f0b083dca28ea26fc49
José Rodolfo <rdfvalerio@gmail.com>
2026-05-14 08:36:06 -0300

    fix: torna flag e logos opcionais no schema de games

---
+++ infra/database/migrations/0007_condemned_dark_phoenix.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "games" ALTER COLUMN "countryFlag" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "games" ALTER COLUMN "leagueLogo" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "games" ALTER COLUMN "homeTeamLogo" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "games" ALTER COLUMN "awayTeamLogo" DROP NOT NULL;
