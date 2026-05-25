410d46902cfe2b5d2dfbb386cebcfc26b51c19fe
José Rodolfo <rdfvalerio@gmail.com>
2026-05-25 17:18:22 -0300

    refactor: remove colunas de imagem dos schemas de games e channels

---
+++ infra/database/migrations/0013_strange_husk.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "channels" DROP COLUMN "logo";--> statement-breakpoint
+ALTER TABLE "games" DROP COLUMN "countryFlag";--> statement-breakpoint
+ALTER TABLE "games" DROP COLUMN "leagueLogo";--> statement-breakpoint
+ALTER TABLE "games" DROP COLUMN "homeTeamLogo";--> statement-breakpoint
+ALTER TABLE "games" DROP COLUMN "awayTeamLogo";
--- infra/database/schema/channels.ts
+++ infra/database/schema/channels.ts
@@ -5,7 +5,6 @@ export const channelsSchema = pgTable(
   {
     id: uuid().defaultRandom().primaryKey(),
     name: varchar({ length: 255 }).notNull(),
-    logo: varchar({ length: 500 }),
     url: varchar({ length: 500 }),
     created_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
   },
